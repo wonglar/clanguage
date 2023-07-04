1bf6580ac2b7411138d7d16178c946a913a1dd0f
zk <zk@163.com>
2023-07-04 13:23:47 +0800

    first commit

---
+++ test_7_3/test_7_3/test.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+// 第一个C语言程序
+
+
+// C语言代码中一定要有main函数
+// 主函数  main函数是程序的入口，main函数有且仅有一个
+int main()
+{
+	// printf是一个库函数，专门用来打印数据的
+	printf("hello worl\n");
+	return 0;
+}
+
+// 古老的写法
+//void main()
+//{
+//
+//}
