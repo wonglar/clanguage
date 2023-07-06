35795e2c1824f783a1fe14e8c74a9714ba6c0fb0
wonglar <wonglar@163.com>
2023-07-06 11:30:48 +0800

    作业练习

---
+++ test_7_6/test_7_6/test.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+// 作业
+//int main()
+//{
+//	printf("Name    Age    Gender\n");
+//	printf("---------------------\n");
+//	printf("Jack    18    man\n");
+//	return 0;
+//}
+
+
+// 0 -- 数字0
+// '0' - 字符 - ASCII码值是48
+// '\0' - 字符 - ASCII码值是0
+
+
+//写一个函数求两个数的较大值
+
+//int getMax(int n1, int n2)
+//{
+//	return n1 > n2 ? n1 : n2;
+//}
+//int main()
+//{
+//	int n1 = 0;
+//	int n2 = 0;
+//	scanf("%d %d", &n1, &n2);
+//	int ret = getMax(n1, n2);
+//	printf("最大值是：%d\n", ret);
+//	return 0;
+//}
