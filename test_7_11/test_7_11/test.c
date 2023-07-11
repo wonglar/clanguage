8d2a022609669e18543cfc646e2ddf898a336626
wonglar <wonglar@163.com>
2023-07-11 16:54:39 +0800

    23.7.11

---
+++ test_7_11/test_7_11/test.c
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+
+// 计算n的阶乘
+//int main()
+//{
+//	int n = 0;
+//	int i = 0;
+//	int ret = 1;
+//	scanf("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		ret *= i;
+//	}
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+
+
+int main()
+{
+
+	return 0;
+}
