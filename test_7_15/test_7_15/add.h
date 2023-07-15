7d33ba7e5bb330b5fb7e9eab55f88e86efca59a3
zk <zk@163.com>
2023-07-15 23:27:23 +0800

    函数的声明与定义

---
+++ test_7_15/test_7_15/add.h
@@ -0,0 +1,4 @@
+﻿#pragma once
+
+// 函数的声明
+int add(int, int);  // int add(int a, int b);
---
+++ test_7_15/test_7_15/test.c
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+// 函数的声明和定义
+
+#include "add.h"
+// #pragma comment(lib, "add.lib");  // 包含动态库
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	scanf("%d %d", &a, &b);
+
+	// 加法
+	int sum = add(a, b);
+	printf("%d\n", sum);
+
+	return 0;
+}
+
+
