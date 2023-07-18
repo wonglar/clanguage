6b5ead938577b9ddb3e23dc7c88ef580716d1c80
wonglar <wonglar@163.com>
2023-07-18 16:31:41 +0800

    数组

---
+++ test_7_18/test_7_18/test.c
@@ -0,0 +1,44 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+// 数组
+
+// 冒泡排序
+//void bubble_sort(int arr[], int sz)
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < sz - 1; i++)
+//	{
+//		for (j = 0; j < sz - 1 - i; j++)
+//		{
+//			if (arr[j] > arr[j + 1])
+//			{
+//				int tmp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = tmp;
+//			}
+//		}
+//	}
+//}
+//void print(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	printf("\n");
+//}
+//int main()
+//{
+//	int arr[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	// 冒泡排序
+//	bubble_sort(arr, sz);
+//
+//	print(arr, sz);
+//
+//	return 0;
+//}
