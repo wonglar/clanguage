dceed6cfcc9e7bbeb763401dc9455eca76dde75a
wonglar <wonglar@163.com>
2023-07-29 16:18:15 +0800

    指针与数组

---
+++ test_7_29/test_7_29/test.c
@@ -0,0 +1,42 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	int arr[10] = {0};
+//	printf("%d\n", &arr[9] - &arr[0]);
+//	// 指针-指针的绝对值得到的是指针之间的元素个数,指向同一块空间的指针才能相减
+//
+//
+//	return 0;
+//}
+
+//int my_strlen(char* str)
+//{
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+
+
+//int my_strlen(char* str)
+//{
+//	char* start = str;
+//	while (*str != '\0')
+//	{
+//		str++;
+//	}
+//	return (str - start);
+//}
+//
+//
+//int main()
+//{
+//	printf("%d\n", my_strlen("abcdef"));
+//	return 0;
+//}
+
+
