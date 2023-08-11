1944b65d2486c5b48107139ce721bb2a8d539974
wonglar <wonglar@163.com>
2023-08-11 23:34:17 +0800

    指针的运算笔试题

---
+++ test_8_11/test_8_11/test.c
@@ -0,0 +1,89 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int main()
+//{
+//	int a[5] = { 1,2,3,4,5 };
+//	int* ptr = (int*)(&a + 1);
+//
+//	printf("%d %d\n", *(a + 1), *(ptr - 1));  // 2 5
+//
+//	return 0;
+//}
+
+
+//int main()
+//
+//{
+//	int a[4] = { 1,2,3,4 };
+//	int* ptr1 = (int*)(&a + 1);
+//	int* ptr2 = (int*)((int)a + 1);
+//	printf("%x, %x\n", ptr1[-1], *ptr2);  // 4, 2000000
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a[3][2] = { (0,1), (2,3), (4, 5) };
+//	int* p;
+//	p = a[0];
+//	printf("%d\n", p[0]);  // 1
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a[5][5];
+//	int(*p)[4];
+//	p = a;
+//	printf("%p, %d\n", &p[4][2] - &a[4][2], &p[4][2] - &a[4][2]);  // FFFFFFFC, -4
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int aa[2][5] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int* ptr1 = (int*)(&aa + 1);
+//	int* ptr2 = (int*)(*(aa + 1));
+//	printf("%d,%d", *(ptr1 - 1), *(ptr2 - 1));  // 10,5
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char* a[] = { "work", "at", "alibaba" };
+//	char** pa = a;
+//	pa++;
+//
+//	printf("%s\n", *pa);  // at
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char* c[] = { "ENTER", "NEW", "POINT", "FIRST" };
+//	char** cp[] = { c + 3, c + 2, c + 1, c };
+//	char*** cpp = cp;
+//
+//	printf("%s\n", **++cpp);
+//	printf("%s\n", *-- * ++cpp + 3);
+//	printf("%s\n", *cpp[-2] + 3);
+//	printf("%s\n", cpp[-1][-1] + 1);
+//
+//	return 0;
+//}
+///*
+//POINT
+//ER
+//ST
+//EW
+//*/
