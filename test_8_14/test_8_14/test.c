b518c458a6e40c0db8da2b5e48ed44d0a7a18393
wonglar <wonglar@163.com>
2023-08-14 16:55:27 +0800

    内存函数

---
+++ test_8_14/test_8_14/test.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <string.h>
+/*
+内存操作函数
+*/
+
+
+// memcpy 内存拷贝
+int main()
+{
+	int arr1[] = {1,2,3,4,5,6,7};
+	int arr2[10] = {0};
+	memcpy(arr2, arr1, sizeof(arr1[0])*7);
+
+	return 0;
+}
