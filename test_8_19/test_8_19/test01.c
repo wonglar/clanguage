8543d3583646d1016214d3249650e368ab7481fe
wonglar <wonglar@163.com>
2023-08-19 23:46:25 +0800

    作业

--- test_8_19/test_8_19/test01.c
+++ test_8_19/test_8_19/test01.c
@@ -63,7 +63,7 @@
 1 1
 1 2 1
 1 3 3 1
-1 4 9 4 1  
+1 4 9 4 1
 */
 
 //int main()
@@ -212,3 +212,173 @@
 //}
 
 
+//int main()
+//{
+//	int arr[50] = { 0 };
+//	int n = 0;
+//	scanf("%d", &n);
+//	int i = 0;
+//	int flag1 = 0;
+//	int flag2 = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//		if (i > 0)
+//		{
+//			if (arr[i] >= arr[i - 1])
+//			{
+//				flag1 = 1;
+//			}
+//			else
+//			{
+//				flag2 = 1;
+//			}
+//		}
+//	}
+//	if (flag1 + flag2 == 1)
+//		printf("sorted\n");
+//	else
+//		printf("unsorted\n");
+//	return 0;
+//}
+
+// 方法一
+//int is_left_move(char arr1[], char arr2[])
+//{
+//	int len = strlen(arr1);
+//	int i = 0;
+//	for (i = 0; i < len; i++)
+//	{
+//		char tmp = arr1[0];
+//		int j = 0;
+//		for (j = 0; j < len - 1; j++)
+//		{
+//			arr1[j] = arr1[j + 1];
+//		}
+//		arr1[len - 1] = tmp;
+//
+//		if (strcmp(arr1, arr2) == 0)
+//			return 1;
+//	}
+//	return 0;
+//}
+
+
+// 方法二
+
+//int is_left_move(char arr1[], char arr2[])
+//{
+//	int len1 = strlen(arr1);
+//	int len2 = strlen(arr2);
+//	if (len1 != len2)
+//		return 0;
+//	strncat(arr1, arr2, len1);
+//	char* ret = strstr(arr1, arr2);
+//	if (ret == NULL)
+//		return 0;
+//	else
+//		return 1;
+//}
+//
+//int main()
+//{
+//	char arr1[20] = "abcdef";
+//	char arr2[] = "cdef";
+//
+//	int ret = is_left_move(arr1, arr2);
+//	if (ret == 1)
+//	{
+//		printf("OK\n");
+//	}
+//	else
+//	{
+//		printf("No\n");
+//	}
+//
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int n = 0;
+//	int m = 0;
+//	scanf("%d %d", &n, &m);
+//
+//	int arr[10][10] = { 0 };
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		for (j = 0; j < m; j++)
+//		{
+//			scanf("%d", &arr[i][j]);
+//		}
+//	}
+//
+//	for (i = 0; i < m; i++)
+//	{
+//		for (j = 0; j < n; j++)
+//		{
+//			printf("%d ", arr[j][i]);
+//		}
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int arr[10][10] = { 0 };
+//	int i = 0;
+//	int j = 0;
+//	int flag = 1;
+//	for (i = 0; i < n; i++)
+//	{
+//		for (j = 0; j < n; j++)
+//		{
+//			scanf("%d", &arr[i][j]);
+//		}
+//	}
+//
+//	for (i = 0; i < n; i++)
+//	{
+//		/*for (j = 0; j < i; j++)
+//		{
+//			if (arr[i][j] != 0)
+//			{
+//				flag = 0;
+//				goto end;
+//			}
+//		}*/
+//
+//		for (j = 0; j < n; j++)
+//		{
+//			if (j < i)
+//			{
+//				if (arr[i][j] != 0)
+//				{
+//					flag = 0;
+//					goto end;
+//				}
+//			}
+//		}
+//	}
+//
+//end:
+//	if (flag == 0)
+//	{
+//		printf("NO\n");
+//	}
+//	else
+//	{
+//		printf("Yes\n");
+//	}
+//	return 0;
+//}
