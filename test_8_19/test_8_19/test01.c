487b08bf5c378fd89001871c43407ca76f5ecea8
wonglar <wonglar@163.com>
2023-08-19 17:05:35 +0800

    作业

---
+++ test_8_19/test_8_19/test01.c
@@ -0,0 +1,214 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	int d = 0;
+//	int e = 0;
+//
+//	for (a = 1; a <= 5; a++)
+//	{
+//		for (b = 1; b <= 5; b++)
+//		{
+//			for (c = 1; c <= 5; c++)
+//			{
+//				for (d = 1; d <= 5; d++)
+//				{
+//					for (e = 1; e <= 5; e++)
+//					{
+//						if ((b == 2) + (a == 3) == 1
+//							&& (b == 2) + (e == 4) == 1
+//							&& (c == 1) + (d == 2) == 1
+//							&& (c == 5) + (d == 3) == 1
+//							&& (e == 4) + (a == 1) == 1)
+//						{
+//							if ((a + b + c + d + e == 15) && (a*b*c*d*e == 120))
+//							{
+//								printf("a = %d, b = %d, c = %d, d = %d, e = %d\n", a, b, c, d, e);
+//							}
+//						}
+//					}
+//				}
+//			}
+//		}
+//	}
+//
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int killer = 0;
+//	for (killer = 'a'; killer <= 'd'; killer++)
+//	{
+//		if ((killer != 'a') + (killer == 'c') + (killer == 'd') + (killer != 'd') == 3)
+//		{
+//			printf("%c\n", killer);
+//		}
+//	}
+//
+//	return 0;
+//}
+
+
+
+/*
+1
+1 1
+1 2 1
+1 3 3 1
+1 4 9 4 1  
+*/
+
+//int main()
+//{
+//	int arr[10][10] = { 0 };
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		for (j = 0; j <= i; j++)
+//		{
+//			if (j == 0)
+//				arr[i][j] = 1;
+//			if (i == j)
+//				arr[i][j] = 1;
+//			if (i >= 2 && j >= 1)
+//			{
+//				arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+//			}
+//			printf("%3d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char a[1000] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 1000; i++)
+//	{
+//		a[i] = -1 - i;
+//
+//	}
+//	printf("%d", strlen(a));
+//	return 0;
+//}
+
+
+// 字符串左旋
+#include <string.h>
+
+//void left_rotate(char arr[], int k)
+//{
+//	int len = strlen(arr);
+//	int i = 0;
+//	k %= len;
+//	for (i = 0; i < k; i++)
+//	{
+//		char tmp = arr[0];
+//		int j = 0;
+//		for (j = 0; j < len - 1; j++)
+//		{
+//			arr[j] = arr[j + 1];
+//		}
+//		arr[len - 1] = tmp;
+//	}
+//}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	int k = 0;
+//	scanf("%d", &k);
+//	left_rotate(arr, k);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+//void reverse(char* left, char* right)
+//{
+//	while (left < right)
+//	{
+//		char tmp = *left;
+//		*left = *right;
+//		*right = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//
+//void left_rotate(char arr[], int k)
+//{
+//	int len = strlen(arr);
+//	k %= len;
+//	reverse(arr, arr + k - 1);
+//	reverse(arr + k, arr + len - 1);
+//	reverse(arr, arr + len - 1);
+//}
+//
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	int k = 0;
+//	scanf("%d", &k);
+//	left_rotate(arr, k);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+// 找到返回1
+//int find_num(int arr[][3], int* px, int* py, int k)
+//{
+//	int x = 0;
+//	int y = *py - 1;
+//	while (x <= *px - 1 && y >= 0)
+//	{
+//		if (k < arr[x][y])
+//		{
+//			y--;
+//		}
+//		else if (k > arr[x][y])
+//		{
+//			x++;
+//		}
+//		else
+//		{
+//			*px = x;
+//			*py = y;
+//			return 1;
+//		}
+//	}
+//
+//	return 0; // 找不到
+//}
+//
+//int main()
+//{
+//	int arr[3][3] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//	int x = sizeof(arr) / sizeof(arr[0]);
+//	int y = sizeof(arr[0]) / sizeof(arr[0][0]);
+//	int k = 0;
+//	scanf("%d", &k);
+//	int ret = find_num(arr, &x, &y, k);
+//	if (ret == 1)
+//	{
+//		printf("找到了,下标是:%d %d\n",x, y);
+//	}
+//	else
+//	{
+//		printf("找不到\n");
+//	}
+//	return 0;
+//}
+
+
