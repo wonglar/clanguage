d4beff9973b393010ee594a757cab2c1467ece14
wonglar <wonglar@163.com>
2023-08-23 17:02:36 +0800

    通讯录

---
+++ test_8_23_Contact/test_8_23_Contact/Contact.c
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "Contact.h"
+
+// 初始化通讯录
+void InitContact(Contact* pc)
+{
+	pc->count = 0;
+	memset(pc->data, 0, sizeof(pc->data));
+}
+
+
+// 添加通讯录
+void AddContact(Contact* pc)
+{
+	if (pc->data == MAX_SIZE)
+	{
+		printf("通讯录已满\n");
+		return;
+	}
+}
+
---
+++ test_8_23_Contact/test_8_23_Contact/test.c
@@ -0,0 +1,53 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "Contact.h"
+
+void menu()
+{
+	printf("*********************************\n");
+	printf("***** 1.add      2.del      *****\n");
+	printf("***** 3.search   4.modify   *****\n");
+	printf("***** 5.show     6.sort     *****\n");
+	printf("*****      0.exit           *****\n");
+	printf("*********************************\n");
+}
+
+
+int main()
+{
+	int input = 0;
+	Contact con;  // 通讯录
+	InitContact(&con); // 初始化通讯录
+	printf("%d\n", sizeof(con.data) / sizeof(con.data[0]));
+	do
+	{
+		menu();
+		printf("请选择:>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			AddContact(&con);
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+		case 0:
+			printf("退出通讯录\n");
+			break;
+		default:
+			printf("选择错误,请重新选择!\n");
+			break;
+		}
+
+	} while (input);
+
+	return 0;
+}
